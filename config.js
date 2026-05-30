e9db1e391b9540ccb08c96d998fb7fef31d400e2
Ritesh Gupta <ritz9690@gmail.com>
2026-05-30 16:23:04 +0530

    config.js remove from git

--- config.js
+++
@@ -1,9 +0,0 @@
-// ============================================================
-//  Flatzy — Secret Config
-//  ⚠️  Yeh file GitHub pe KABHI push mat karo!
-//  .gitignore mein "config.js" add karo
-// ============================================================
-
-const CLOUD_NAME       = 'dhl8byas2';
-const CLOUD_API_KEY    = '948666951824159';    // Cloudinary dashboard se
-const CLOUD_API_SECRET = 'CpLeXj5I2oN_hXcWBuDD7Sz5peo'; // Cloudinary dashboard se
